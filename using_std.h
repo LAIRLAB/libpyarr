9fd8354f06840de241cdbb1ac87f1b79dc38ab76
Elliot Cuzzillo <ecuzzillo@gmail.com>
2013-06-13 11:08:57 -0700

    (split) forgot file

---
+++ using_std.h
@@ -0,0 +1,17 @@
+#ifndef USING_STD_H
+#define USING_STD_H
+
+using std::vector;
+using std::endl;
+using std::cout;
+using std::string;
+using std::stringstream;
+using namespace std::tr1;
+using std::ifstream;
+using std::pair;
+using std::make_pair;
+using std::ofstream;
+using std::cerr;
+using std::max;
+
+#endif
