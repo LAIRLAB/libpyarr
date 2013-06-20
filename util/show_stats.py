7408423e81a2910a222b387a0889f3d820807049
Elliot Cuzzillo <ecuzzillo@gmail.com>
2013-06-20 13:13:22 -0700

    (split) underflow?

--- util/show_stats.py
+++ util/show_stats.py
@@ -1,7 +1,8 @@
-# Usage: python show_stats.py foo.prof
 import sys
 import hotshot, hotshot.stats
-stats = hotshot.stats.load(sys.argv[1])
-stats.strip_dirs()
-stats.sort_stats('time', 'calls')
-stats.print_stats(50)
+
+def main():
+    stats = hotshot.stats.load(sys.argv[1])
+    stats.strip_dirs()
+    stats.sort_stats('time', 'calls')
+    stats.print_stats(50)
