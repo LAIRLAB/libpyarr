a7462f56b17823d7d4da0c51e573e167e93a86d3
Nick Rhinehart <nrhine1@gmail.com>
2013-07-17 12:43:54 -0400

    (split) help string hacking into the argparse woah v. cool

--- util/config_util.py
+++ util/config_util.py
@@ -9,14 +9,28 @@ def cast(s):
 
 class ConfigOverrider(object):
     def __init__(self, config):
-        self.p = argparse.ArgumentParser()
+        self.p = argparse.ArgumentParser(formatter_class = \
+                                             argparse.ArgumentDefaultsHelpFormatter)
+        self.help = {}
+
+        for (k, v) in config.items('Help'):
+            self.help[k] = v
+
         for s in config.sections():
+            if s.lower() == 'help':
+                continue
+
             for (k, v) in config.items(s):
                 d = cast(v)
                 if isinstance(d, bool):
                     action = 'store_true'
                 else:
                     action = 'store'
+
+                if k not in self.help:
+                    self.help[k] = ''
+
                 self.p.add_argument('--{}'.format(k.replace('_', '-')), 
                                     default = d, 
-                                    action = action)
+                                    action = action,
+                                    help = self.help[k])
