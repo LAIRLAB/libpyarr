61bcaacb711ab9111b79a07e5f56764d18e6837b
Elliot Cuzzillo <ecuzzillo@gmail.com>
2014-01-23 18:17:37 -0800

    env conciseness, readme fix

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 
 pathadd() {
     if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
