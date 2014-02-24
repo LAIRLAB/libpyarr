eb7b2695011799f612f6c7a7f28d20adb9b7ac32
Nick Rhinehart <nrhine1@gmail.com>
2014-02-23 20:36:19 -0500

    l2v addition

--- boost_util.py
+++ boost_util.py
@@ -1,3 +1,7 @@
+import numpy
+
+import libboost_common
+
 def v2l(v):
     l = []
     if hasattr(v, 'at'):
@@ -10,3 +14,18 @@ def v2l(v):
     for i in xrange(len(v)):
         l.append(getattr(v, atf)(i))
     return l
+
+def l2v(l, t):
+    if t == numpy.float64:
+        v = libboost_common.pyarr_double_vec()
+        for i in l:
+            if not isinstance(i, numpy.ndarray):
+                raise RuntimeError("list entry not ndarray")
+            if i.dtype != t:
+                raise RuntimeError("list entry type not {}".format(t))
+            v.append(i)
+    else:
+        raise RuntimeError("boost_util.l2v unrecognized type: {}".format(v))
+    return v
+
+
